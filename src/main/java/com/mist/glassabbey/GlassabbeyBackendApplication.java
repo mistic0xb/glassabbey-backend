87115e2e799bb9ddb9666829d18ae5183d1c804c
Mist <mistic0xb@gmail.com>
2026-05-26 20:01:53 +0530

    Initial commit

---
+++ src/main/java/com/mist/glassabbey/GlassabbeyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mist.glassabbey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlassabbeyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlassabbeyBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mist/glassabbey/GlassabbeyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mist.glassabbey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlassabbeyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
