700db9953c01483c74b1511373e4583772438ae0
mist <mistic0xb@gmail.com>
2026-08-22 09:16:29 +0530

    fix: removed specific fields of pieceDto & galleryDto and included entire dtos of both

--- src/main/java/com/mist/glassabbey/auction/AuctionMapper.java
+++ src/main/java/com/mist/glassabbey/auction/AuctionMapper.java
@@ -1,14 +1,11 @@
 package com.mist.glassabbey.auction;
 
 import com.mist.glassabbey.auction.dtos.AuctionDto;
+import com.mist.glassabbey.gallery.GalleryMapper;
+import com.mist.glassabbey.piece.PieceMapper;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.ReportingPolicy;
 
-@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+@Mapper(componentModel = "spring", uses = {PieceMapper.class, GalleryMapper.class})
 public interface AuctionMapper {
-    @Mapping(target = "pieceId", source = "piece.id")
-    @Mapping(target = "galleryId", source = "gallery.id")
-    @Mapping(target = "status", source = "status")
     AuctionDto toDto(Auction auction);
 }
