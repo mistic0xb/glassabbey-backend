6418d5cb59d8e39589702dbb8945147b6dab4ef0
mist <mistic0xb@gmail.com>
2026-08-23 19:06:47 +0530

    feat: main starting point file

--- src/main/java/com/mist/glassabbey/GlassabbeyBackendApplication.java
+++ src/main/java/com/mist/glassabbey/GlassabbeyBackendApplication.java
@@ -3,8 +3,12 @@ package com.mist.glassabbey;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableScheduling
+@EnableTransactionManagement
 @ConfigurationPropertiesScan
 public class GlassabbeyBackendApplication {
 
